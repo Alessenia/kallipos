adbc6697e7412200aa3a832cfb0d980b4c81120f
Alessenia <92642163+Alessenia@users.noreply.github.com>
2022-11-13 22:10:31 +0200

    Update note.lua

--- note.lua
+++ note.lua
@@ -5,7 +5,7 @@ function Image(img)
     f:close()
     local caption = pandoc.utils.stringify(doc.meta.caption) or "caption has not been set"
     local student = pandoc.utils.stringify(doc.meta.student) or "Student has not been set"
-    local id = pandoc.utils.stringify(doc.meta.id) or "Student ID has not been set"
+    local id = pandoc.utils.stringify(doc.meta.id) or "Student's ID has not been set"
     local comment = "> " .. caption .. " Student Name:" .. student .. " AM: " .. id
     return pandoc.RawInline('markdown',comment)
   end
